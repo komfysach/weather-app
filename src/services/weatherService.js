58a077a4ea0f39d635bcf97363f65b38ec593996
komfysach <sach@getkomfy.net>
2022-11-09 09:31:08 +0100

    build complete

--- src/services/weatherService.js
+++ src/services/weatherService.js
@@ -1,6 +1,6 @@
 import { DateTime } from "luxon";
 
-const API_KEY = "aa8acec25da68b38abf1072fafec72fc"
+const API_KEY = process.env.API_KEY
 const BASE_URL = "https://api.openweathermap.org/data/2.5"
 
 const getWeatherData = (infoType, searchParams) => {
